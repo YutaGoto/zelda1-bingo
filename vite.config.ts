59768293bf7dcf6522c55fb290d81842b2eff433
YutaGoto <you.goto.510@gmail.com>
2023-10-23 22:38:45 +0900

    :wrench: add Repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/zelda1-bingo/',
   build: {
     outDir: 'dist',
     assetsDir: '',
