c4880731eca9831ed59557882d4ed4b568072dc3
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2023-12-28 14:51:57 +0900

    :white_check_mark: add sample test

---
+++ src/ui/Cell.test.tsx
@@ -0,0 +1,22 @@
+  import { render, screen } from "@testing-library/react"
+import {expect, test, vi} from "vitest"
+
+import { Cell } from "./Cell"
+
+test("Cell false", () => {
+  render(<Cell
+    task={{
+      name: {
+        ja: "てすと",
+        en: "test",
+      },
+      category: "demo",
+    }}
+    hit={false}
+    lang="en"
+    messageLang="en"
+    onClick={() => {}}
+  />)
+
+  expect(screen.getByText("test")).toBeTruthy()
+})
