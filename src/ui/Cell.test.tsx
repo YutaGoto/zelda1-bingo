0d92a82ae03723967fe3401dddba1beecdc99621
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2023-12-28 14:53:43 +0900

    :construction_worker: add vitest action

--- src/ui/Cell.test.tsx
+++ src/ui/Cell.test.tsx
@@ -1,22 +1,24 @@
-  import { render, screen } from "@testing-library/react"
-import {expect, test, vi} from "vitest"
+import { render, screen } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
 
-import { Cell } from "./Cell"
+import { Cell } from "./Cell";
 
 test("Cell false", () => {
-  render(<Cell
-    task={{
-      name: {
-        ja: "てすと",
-        en: "test",
-      },
-      category: "demo",
-    }}
-    hit={false}
-    lang="en"
-    messageLang="en"
-    onClick={() => {}}
-  />)
+  render(
+    <Cell
+      task={{
+        name: {
+          ja: "てすと",
+          en: "test",
+        },
+        category: "demo",
+      }}
+      hit={false}
+      lang="en"
+      messageLang="en"
+      onClick={() => {}}
+    />,
+  );
 
-  expect(screen.getByText("test")).toBeTruthy()
-})
+  expect(screen.getByText("test")).toBeTruthy();
+});
