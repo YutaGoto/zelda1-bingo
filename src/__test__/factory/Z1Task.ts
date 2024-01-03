c020442071322f22de2f63315321bed98eee39cc
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-01-03 18:25:06 +0900

    :white_check_mark: fix ui test for new type

---
+++ src/__test__/factory/Z1Task.ts
@@ -0,0 +1,20 @@
+import { Z1Task } from "../../types/Z1Task";
+
+export const factoryZ1Task: Z1Task = {
+  name: {
+    en: "test",
+    ja: "テスト",
+  },
+  category: "test",
+  count: 1,
+  score: 2,
+};
+
+export const factoryZ1MessageTask = {
+  name: {
+    en: "test",
+    ja: "テスト",
+  },
+  category: "message",
+  score: 2,
+};
