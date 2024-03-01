61dfbe089b89c59eda94f88db26c874812035e23
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-03-01 19:38:56 +0900

    :white_check_mark: add ui test

---
+++ src/ui/__test__/CountdownTimer.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { expect, test } from "vitest";
+
+import { CountdownTimer } from "../CountdownTimer";
+
+const setup = () => {
+  const user = userEvent.setup();
+  return { user };
+};
+
+test("CountdownTimer", () => {
+  render(<CountdownTimer />);
+  expect(screen.getByText("Start")).toBeTruthy();
+  expect(screen.getByText("Pause")).toBeTruthy();
+  expect(screen.getByText("Reset")).toBeTruthy();
+});
+
+test("Snapshot", () => {
+  const { asFragment } = render(<CountdownTimer />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("Start", async () => {
+  const { user } = setup();
+  render(<CountdownTimer />);
+  await user.click(screen.getByText("Start"));
+  await new Promise((r) => setTimeout(r, 2000));
+  expect(screen.queryByText("0:15:00")).toBeNull();
+});
