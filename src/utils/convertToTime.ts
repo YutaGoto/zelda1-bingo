2ceea28e1eef653e4e746907f0cb8857f75f8cee
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-03-02 12:36:24 +0900

    :white_check_mark: add convertToTimeTest

--- src/utils/convertToTime.ts
+++ src/utils/convertToTime.ts
@@ -9,8 +9,8 @@ export const convertToTime = ({
 }: ConvertToTime): string => {
   const milliseconds = `00${time % 1000}`.slice(-3);
   const seconds = `0${Math.floor(time / 1000) % 60}`.slice(-2);
-  const minutes = `0${Math.floor(time / 60000)}`.slice(-2);
-  const hours = Math.floor(time / 3600000);
+  const minutes = `0${Math.floor(time / 60000) % 60}`.slice(-2);
+  const hours = Math.floor(time / 3600000) % 24;
 
   if (isMillisecond) {
     return `${hours}:${minutes}:${seconds}.${milliseconds}`;
