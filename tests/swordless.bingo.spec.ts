f9ec4cbcc8be689cb1dd58fbdcebef32a3812ce5
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-04-20 10:51:22 +0900

    :white_check_mark: add 2Q and swordless e2e testing

--- tests/swordless.bingo.spec.ts
+++ tests/swordless.bingo.spec.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "@playwright/test";
 
-test.describe("firstQuest bingo", () => {
+test.describe("swordless bingo", () => {
   test.describe("i18n", () => {
-    test("show firstQuest bingo, English", async ({ page }) => {
-      await page.goto("/firstQuest/en");
+    test("show swordless bingo, English", async ({ page }) => {
+      await page.goto("/swordless/en");
 
-      await expect(page).toHaveURL(/\/firstQuest\/en/);
+      await expect(page).toHaveURL(/\/swordless\/en/);
       await expect(page).toHaveTitle("The Legend of Zelda Bingo");
 
       const contentTitle = page.locator("h1");
@@ -15,10 +15,10 @@ test.describe("firstQuest bingo", () => {
       await expect(categoryTitle).toHaveText("First Quest");
     });
 
-    test("show firstQuest bingo, Japanese", async ({ page }) => {
-      await page.goto("/firstQuest/ja");
+    test("show swordless bingo, Japanese", async ({ page }) => {
+      await page.goto("/swordless/ja");
 
-      await expect(page).toHaveURL(/\/firstQuest\/ja/);
+      await expect(page).toHaveURL(/\/swordless\/ja/);
       await expect(page).toHaveTitle("The Legend of Zelda Bingo");
 
       const contentTitle = page.locator("h1");
@@ -31,62 +31,62 @@ test.describe("firstQuest bingo", () => {
 
   test("show current seed", async ({ page }) => {
     const seed = "1234";
-    await page.goto(`/firstQuest/en?seed=${seed}`);
+    await page.goto(`/swordless/en?seed=${seed}`);
 
-    await expect(page).toHaveURL(/\/firstQuest\/en/);
+    await expect(page).toHaveURL(/\/swordless\/en/);
     const seedBadge = page.locator("span.chakra-badge");
     await expect(seedBadge).toHaveText(seed);
   });
 
   test.describe("update seed", () => {
     test("update seed", async ({ page }) => {
-      await page.goto("/firstQuest/en");
+      await page.goto("/swordless/en");
 
-      await expect(page).toHaveURL(/\/firstQuest\/en/);
+      await expect(page).toHaveURL(/\/swordless\/en/);
       await page.getByLabel("Seed").clear();
       await page.getByLabel("Seed").fill("1234");
       await page.click('button:has-text("Update Seed")');
 
-      await expect(page).toHaveURL(/\/firstQuest\/en\?seed=1234/);
+      await expect(page).toHaveURL(/\/swordless\/en\?seed=1234/);
     });
 
     test("reset seed", async ({ page }) => {
-      const firstSeed = "1234";
-      await page.goto(`/firstQuest/en?seed=${firstSeed}`);
+      const secondSeed = "1234";
+      await page.goto(`/swordless/en?seed=${secondSeed}`);
 
-      await expect(page).toHaveURL(/\/firstQuest\/en\?seed=1234/);
+      await expect(page).toHaveURL(/\/swordless\/en\?seed=1234/);
       await page.click('button:has-text("Reset Seed")');
 
       const newUrl = new URL(page.url());
       const params = newUrl.searchParams;
       const seed = params.get("seed");
-      expect(seed).not.toEqual(firstSeed);
+      expect(seed).not.toEqual(secondSeed);
     });
   });
 
   test.describe("switch category", () => {
-    test("switch to secondQuest", async ({ page }) => {
-      await page.goto("/firstQuest/en");
+    test("switch to firstQuest", async ({ page }) => {
+      await page.goto("/swordless/en");
+
+      await expect(page).toHaveURL(/\/swordless\/en/);
+      await page.click('a:has-text("1st Quest")');
+      await page.waitForURL(/\/firstQuest\/en/);
 
       await expect(page).toHaveURL(/\/firstQuest\/en/);
+      const categoryTitle = page.locator("h5");
+      await expect(categoryTitle).toHaveText("First Quest");
+    });
+
+    test("switch to secondQuest", async ({ page }) => {
+      await page.goto("/swordless/en");
+
+      await expect(page).toHaveURL(/\/swordless\/en/);
       await page.click('a:has-text("2nd Quest")');
       await page.waitForURL(/\/secondQuest\/en/);
 
       await expect(page).toHaveURL(/\/secondQuest\/en/);
       const categoryTitle = page.locator("h5");
       await expect(categoryTitle).toHaveText("Second Quest");
-    });
-
-    test("switch to swordless", async ({ page }) => {
-      await page.goto("/firstQuest/en");
-
-      await expect(page).toHaveURL(/\/firstQuest\/en/);
-      await page.click('a:has-text("Swordless")');
-      await page.waitForURL(/\/swordless\/en/);
-
-      await expect(page).toHaveURL(/\/swordless\/en/);
-      const categoryTitle = page.locator("h5");
-      await expect(categoryTitle).toHaveText("Swordless");
     });
   });
 });
