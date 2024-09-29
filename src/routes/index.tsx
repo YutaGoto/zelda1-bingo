8e1f164ee99deb4f790129ef21bcea4ee6454e5d
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-09-29 13:17:06 +0900

    :recycle: add routes directory

---
+++ src/routes/index.tsx
@@ -0,0 +1,41 @@
+import {
+  Navigate,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import App from "../App";
+import Score from "../Score";
+
+const url = new URL(location.href);
+const params = new URLSearchParams(url.search);
+const paramsSeed = params.get("seed");
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Outlet,
+    children: [
+      {
+        path: "/score/:category/:lang",
+        Component: Score,
+      },
+      {
+        path: "/:category/:lang",
+        Component: App,
+      },
+      {
+        path: "/:lang",
+        element: <Navigate to={`/firstQuest/en?seed=${paramsSeed}`} />,
+      },
+      {
+        path: "/",
+        element: <Navigate to={`/firstQuest/en?seed=${paramsSeed}`} />,
+      },
+    ],
+  },
+]);
+
+export const Z1BingoRoutes = () => {
+  return <RouterProvider router={router} />;
+};
