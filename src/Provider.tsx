057bdc84b7c379e86c80dbeed0dcbfc6df614de8
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-12-02 22:01:03 +0900

    :adhesive_bandage: fix provider

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { ThemeProvider } from "next-themes";
 import type { ReactNode } from "react";
+
 import { system } from "./theme";
 
 interface ProviderProps {
@@ -8,15 +8,5 @@ interface ProviderProps {
 }
 
 export const Provider = ({ children }: ProviderProps) => {
-  return (
-    <ChakraProvider value={system}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem={true}
-      >
-        {children}
-      </ThemeProvider>
-    </ChakraProvider>
-  );
+  return <ChakraProvider value={system}>{children}</ChakraProvider>;
 };
