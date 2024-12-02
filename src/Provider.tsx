7c8dd94e52e1137935e32522aeed9b0a35ad3a58
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-12-02 22:10:04 +0900

    :adhesive_bandage: fix theme

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,12 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { ReactNode } from "react";
 
-import { system } from "./theme";
+import { ColorModeProvider } from "./components/ui/color-mode";
+import { system, theme } from "./theme";
 
 interface ProviderProps {
   children: ReactNode;
 }
 
 export const Provider = ({ children }: ProviderProps) => {
-  return <ChakraProvider value={system}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider value={system}>
+      <ColorModeProvider theme={theme}>{children}</ColorModeProvider>
+    </ChakraProvider>
+  );
 };
--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,20 @@
 import { createSystem, defaultConfig } from "@chakra-ui/react";
 
-export const system = createSystem(defaultConfig, {
-  theme: {
-    tokens: {
-      fonts: {
-        heading: {
-          value: `'Noto Sans JP Variable', sans-serif`,
-        },
-        body: {
-          value: `'Noto Sans JP Variable', sans-serif`,
-        },
+export const theme = {
+  tokens: {
+    fonts: {
+      heading: {
+        value: `'Noto Sans JP Variable', sans-serif`,
+      },
+      body: {
+        value: `'Noto Sans JP Variable', sans-serif`,
       },
     },
+  },
+};
+
+export const system = createSystem(defaultConfig, {
+  theme: {
+    ...theme,
   },
 });
